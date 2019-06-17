3ac4d01aa3a98ac73e25e88eb2fa10c6d1c7cbb0
Jean-Baptiste Delcros <jbdelcros4@hotmail.fr>
2019-06-17 15:19:34 +0200

    Initial Commit

---
+++ Projet3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Projet3
+//
+//  Created by DELCROS Jean-baptiste on 17/06/2019.
+//  Copyright © 2019 DELCROS Jean-baptiste. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
